30e41fd5b6811806c90711f2ee56565209dc294d
jiyancarloo <166023368+jiyancarloo@users.noreply.github.com>
2024-07-24 20:05:07 +0800

    added Portal

--- PLP SET/src/main.js
+++ PLP SET/src/main.js
@@ -4,5 +4,7 @@ import { createApp } from 'vue'
 // add this
 import './index.css'
 import Login from './Login.vue'
+import Portal from './Portal.vue'
 
-createApp(Login).mount('#app')
+//createApp(Login).mount('#app')
+createApp(Portal).mount('#app')
