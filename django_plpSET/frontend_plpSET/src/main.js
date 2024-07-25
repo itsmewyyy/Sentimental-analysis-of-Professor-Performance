273e98d27955774e8504bc57ab1cbfaab23ecf28
Ryan Celestino Intalan <ryanintalan@gmail.com>
2024-07-25 20:11:20 +0800

    training data updates

--- django_plpSET/frontend_plpSET/src/main.js
+++ django_plpSET/frontend_plpSET/src/main.js
@@ -1,9 +1,8 @@
-import { createApp } from 'vue'
+import { createApp } from "vue";
 
-import './index.css'
-import portal from './portal.vue'
-import login_Admin from './login_Admin.vue'
-import login_Student from './login_Student.vue'
+import "./index.css";
+import portal from "./portal.vue";
+import login_Admin from "./login_Admin.vue";
+import login_Student from "./login_Student.vue";
 
-
-createApp(login_Student).mount('#app')
+createApp(login_Student).mount("#app");
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\acer\Documents\GitHub\Sentimental-analysis-of-Professor-Performance\venv
+set VIRTUAL_ENV=C:\Users\Wayan\Desktop\Sentimental-analysis-of-Professor-Performance\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
