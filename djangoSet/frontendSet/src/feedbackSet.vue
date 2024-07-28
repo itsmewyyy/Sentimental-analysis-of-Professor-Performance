4a03a4210c404b450b36fde719cb29e38e80205f
Ryan Celestino Intalan <ryanintalan@gmail.com>
2024-07-28 22:33:42 +0800

    frontend edits

--- djangoSet/frontendSet/src/feedbackSet.vue
+++ djangoSet/frontendSet/src/feedbackSet.vue
@@ -17,67 +17,60 @@
             <h1>You are currently evaluating</h1>
             <h1 class="font-bold text-plpgreen-400">Riegie D. Tan, DIT</h1>
           </div>
-          <p class="text-sm font-thin text-darks-800">
-            Want to evaluate a different instructor? Select a course from the dropdown menu to display the corresponding professor.
+          <p class="text-sm font-normal text-darks-200/80">
+            Want to evaluate a different instructor? Select a course from the
+            dropdown menu and the corresponding professor will be displayed
+            automatically.
           </p>
         </div>
 
         <form action="">
           <div class="space-y-4">
-          <div class="flex flex-col items-center space-y-12">
-            <CardFeedback>
-            <template #content>
-              <TextAreaComponent
-                id="editor"
-                label="1. How would you describe your experience in this class under him/her?"
-              
-              />
-              <TextAreaComponent
-                id="editor"
-                label="2. What are the 3 words that best describe your teacher/instructor?"
-              
-              />
-              <TextAreaComponent
-                id="editor"
-                label="3. What do you think are the areas of improvement of this teacher/instructor in terms of personal compotency and personality?"
-              
-              />
-              <TextAreaComponent
-                id="editor"
-                label="4. What do you think are the areas of improvement of this teacher/instructor in terms of professional competence?"
-              
-              />
-              <TextAreaComponent
-                id="editor"
-                label="5. Is there something else you think we should have asked you in this survey? What could it be? Please let us know."
-              
-              />
+            <div class="flex flex-col items-center space-y-12">
+              <CardFeedback>
+                <template #content>
+                  <TextAreaComponent
+                    id="editor"
+                    label="1. How would you describe your experience in this class under him/her?"
+                  />
+                  <TextAreaComponent
+                    id="editor"
+                    label="2. What are the 3 words that best describe your teacher/instructor?"
+                  />
+                  <TextAreaComponent
+                    id="editor"
+                    label="3. What do you think are the areas of improvement of this teacher/instructor in terms of personal compotency and personality?"
+                  />
+                  <TextAreaComponent
+                    id="editor"
+                    label="4. What do you think are the areas of improvement of this teacher/instructor in terms of professional competence?"
+                  />
+                  <TextAreaComponent
+                    id="editor"
+                    label="5. Is there something else you think we should have asked you in this survey? What could it be? Please let us know."
+                  />
+                </template>
+              </CardFeedback>
+            </div>
 
-            </template>
-          </CardFeedback>
+            <button
+              type="button"
+              class="text-plpgreen-400 bg-white border border-plpgreen-100 focus:outline-none hover:bg-darks-50 focus:ring-4 focus:ring-gray-100 font-semibold rounded-lg text-sm px-12 py-2.5 me-2 mb-2"
+            >
+              Back
+            </button>
+            <button
+              type="button"
+              class="text-white bg-plpgreen-200 hover:bg-plpgreen-400 focus:ring-4 focus:ring-gray-100 font-semibold rounded-lg text-sm px-12 py-2.5 me-2 mb-2"
+            >
+              Submit
+            </button>
           </div>
-
-          <button
-            type="button"
-            class="text-plpgreen-400 bg-white border border-plpgreen-100 focus:outline-none hover:bg-darks-50 focus:ring-4 focus:ring-gray-100 font-semibold rounded-lg text-sm px-12 py-2.5 me-2 mb-2"
-          >
-            Back
-          </button>
-          <button
-            type="button"
-            class="text-white bg-plpgreen-200 hover:bg-plpgreen-400 focus:ring-4 focus:ring-gray-100 font-semibold rounded-lg text-sm px-12 py-2.5 me-2 mb-2"
-          >
-            Submit
-          </button>
-        </div>
-
         </form>
-      
       </div>
     </section>
   </section>
 </template>
-
 
 <script>
 import CardFeedback from "./components/cardFeedback.vue";
@@ -91,9 +84,7 @@ export default {
 };
 </script>
 
-
 <style scoped>
-
 .w-collapsed-sidebar {
   width: 102px;
 }
--- djangoSet/frontendSet/src/numericalSet.vue
+++ djangoSet/frontendSet/src/numericalSet.vue
@@ -17,34 +17,36 @@
             <h1>You are currently evaluating</h1>
             <h1 class="font-bold text-plpgreen-400">Riegie D. Tan, DIT</h1>
           </div>
-          <p class="text-sm font-thin text-darks-800">
-            Want to evaluate a different instructor? Select a course from the dropdown menu to display the corresponding professor.
+          <p class="text-sm font-normal text-darks-200/80">
+            Want to evaluate a different instructor? Select a course from the
+            dropdown menu and the corresponding professor will be displayed
+            automatically.
           </p>
         </div>
 
-
         <form action="">
           <div class="space-y-4">
-          <div class="flex flex-col items-center space-y-8">
-            <cardComponent
-              header="B. The use of social media platforms (Ex. Facebook, Google Classroom, and etc.) "
-            >
-              <template #content>
-                <questioncomponent body="Artculates course policies blablabalab and babla balab?" />
-                <questioncomponent body="Question 2" />
-              </template>
-            </cardComponent>
-          </div>
+            <div class="flex flex-col items-center space-y-8">
+              <cardComponent
+                header="B. The use of social media platforms (Ex. Facebook, Google Classroom, and etc.) "
+              >
+                <template #content>
+                  <questioncomponent
+                    body="Artculates course policies blablabalab and babla balab?"
+                  />
+                  <questioncomponent body="Question 2" />
+                </template>
+              </cardComponent>
+            </div>
 
-          <button
-            type="button"
-            class="text-plpgreen-400 bg-white border border-plpgreen-100 focus:outline-none hover:bg-plpgreen-400 hover:text-white focus:ring-4 focus:ring-gray-100 font-semibold rounded text-sm px-12 py-2.5 me-2 mb-2"
-          >
-            Next
-          </button>
-        </div>
+            <button
+              type="button"
+              class="text-plpgreen-400 bg-white border border-plpgreen-100 focus:outline-none hover:bg-plpgreen-400 hover:text-white focus:ring-4 focus:ring-gray-100 font-semibold rounded text-sm px-12 py-2.5 me-2 mb-2"
+            >
+              Next
+            </button>
+          </div>
         </form>
-      
       </div>
     </section>
   </section>
