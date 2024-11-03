eaec5fa68214fb7110418d8ba546e002edcb0dc8
Ryan Celestino Intalan <ryanintalan@gmail.com>
2024-11-04 02:16:32 +0800

    asdasd

--- plpSET/backendSet/settings.py
+++ plpSET/backendSet/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOW_ALL_ORIGINS = True
 ALLOWED_HOSTS = ["sentiment-professor-feedback-1.onrender.com"]
 
 CORS_ALLOWED_ORIGINS = ["https://sentiment-professor-feedback.vercel.app", "http://localhost:5173"]  
