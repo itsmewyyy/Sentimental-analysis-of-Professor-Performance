ba5c81b43841018855ae4fbd4851ca131538b101
Ryan Celestino Intalan <ryanintalan@gmail.com>
2024-11-03 17:35:22 +0800

    celery updates

--- plpSET/backendSet/celery.py
+++ plpSET/backendSet/celery.py
@@ -1,17 +1,25 @@
 from __future__ import absolute_import, unicode_literals
 import os
+
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backendSet.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tbc.settings')
 
-# Create the Celery app
-app = Celery('backendSet')
+app = Celery('backendSet',
+             backend=os.getenv('REDIS_URL'),
+             broker=os.getenv('REDIS_URL')
+             )
 
-# Load task modules from all registered Django app configs.
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Autodiscover tasks from your installed apps
+# Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
-
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
