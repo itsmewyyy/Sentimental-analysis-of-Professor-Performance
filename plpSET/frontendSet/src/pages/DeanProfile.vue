bab54ff10428aa01f5a15e7777f7c66104ef65a7
Ryan Celestino Intalan <ryanintalan@gmail.com>
2024-11-12 10:25:42 +0800

    Update DeanProfile.vue

--- plpSET/frontendSet/src/pages/DeanProfile.vue
+++ plpSET/frontendSet/src/pages/DeanProfile.vue
@@ -120,6 +120,7 @@ import { Label } from "@/components/ui/label";
 import { useAuthStore } from "@/store/adminStore";
 import ScrollArea from "@/components/ui/scroll-area/ScrollArea.vue";
 import NavBarDean from "@/components/navigation/NavBarDean.vue";
+import { ref, onMounted } from "vue";
 const authStore = useAuthStore();
 authStore.restoreSession();
 
